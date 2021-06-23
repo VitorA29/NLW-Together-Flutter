330cad12372480c7b787562ba08b87e2dfd2fb05
VitorA29 <vitor_araujo.29@outlook.com>
2021-06-22 21:26:32 -0300

    Initializing the Pay Flow App

---
+++ payflow/lib/shared/app_strings.pt_BR.dart
@@ -0,0 +1,5 @@
+class AppStrings
+{
+  static const String loginPageDescriptionText = "Organize seus boletos em um só lugar";
+  static const String socialLoginButtonGoogle = "Entrar com Google";
+}
