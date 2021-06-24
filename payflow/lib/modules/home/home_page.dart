5f116bb167b610251da9ee325bcda0a0b28c1c01
VitorA29 <vitor_araujo.29@outlook.com>
2021-06-23 21:43:45 -0300

    Starting home page

--- payflow/lib/modules/home/home_page.dart
+++ payflow/lib/modules/home/home_page.dart
@@ -1,15 +1,115 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget
+import 'home_controller.dart';
+import '../../shared/app_strings.pt_BR.dart';
+import '../../shared/themes/app_colors.dart';
+import '../../shared/themes/app_text_styles.dart';
+
+class HomePage extends StatefulWidget
 {
   const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+{
+  final homeController = HomeController();
+  final pages = [
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+  ];
+
+  void setPage(int index)
+  {
+    homeController.setPage(index);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context)
   {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("HomePage"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: AppStrings.appBarUserNameTitle,
+                  style: AppTextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                      text: "Nome",
+                      style: AppTextStyles.titleBoldBackground,
+                    )
+                  ],
+                )
+              ),
+              subtitle: Text(
+                AppStrings.appBarSubtitle,
+                style: AppTextStyles.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[homeController.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: Icon(
+                Icons.home,
+                color: AppColors.primary,
+              ),
+              onPressed: () {
+                setPage(0);
+              },
+            ),
+            GestureDetector(
+              onTap: () {
+                setPage(1);
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Icon(
+                    Icons.add_box_outlined,
+                    color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              ),
+              onPressed: () {
+                setPage(2);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- payflow/lib/shared/app_strings.pt_BR.dart
+++ payflow/lib/shared/app_strings.pt_BR.dart
@@ -3,4 +3,6 @@ class AppStrings
   static const String failInitializingFirebase = "Não foi possível inicializar o Firebase";
   static const String loginPageDescriptionText = "Organize seus boletos em um só lugar";
   static const String socialLoginButtonGoogle = "Entrar com Google";
+  static const String appBarUserNameTitle = "Ola, ";
+  static const String appBarSubtitle = "Mantenha suas contas em dia";
 }
